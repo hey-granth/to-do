8116305b8ce478071f59f310ce6d538caeebcbc7
Granth Agarwal <heygranth@gmail.com>
2025-05-27 20:24:21 +0530

    fixed secret key

--- to_do/settings.py
+++ to_do/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from os import getenv
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = config("DJANGO_SECRET_KEY", default='django-insecure-fallback-for-development')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
